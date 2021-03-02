9c400be0a4b84a973f0492aaa4f347ef31e45eda
Nikolas Vasconcelos <nkvdamian@gmail.com>
2021-03-02 16:23:08 -0300

    Sign up tests catching errors

--- src/__tests__/AppAuth.test.tsx
+++ src/__tests__/AppAuth.test.tsx
@@ -99,6 +99,97 @@ describe('App Authentication', () => {
       expect(res.container).toMatchSnapshot();
     });
 
+    it('catches empty fields', async () => {
+      render(<App />);
+      fireEvent.input(screen.getByPlaceholderText('User'), {
+        target: {
+          value: ``,
+        },
+      });
+      fireEvent.input(screen.getByPlaceholderText('Password'), {
+        target: {
+          value: ``,
+        },
+      });
+      fireEvent.input(screen.getByPlaceholderText('Confirm Password'), {
+        target: {
+          value: ``,
+        },
+      });
+      fireEvent.click(screen.getByText('Sign Up'));
+      await waitFor(() => screen.getByText('Sign Up'));
+      expect(screen.getAllByText(ERROR_MESSAGES.REQUIRED_FIELD).length).toBe(3);
+    });
+
+    it('catches invalid email', async () => {
+      render(<App />);
+      fireEvent.input(screen.getByPlaceholderText('User'), {
+        target: {
+          value: `wrong_username`,
+        },
+      });
+      fireEvent.input(screen.getByPlaceholderText('Password'), {
+        target: {
+          value: VALID_PASSWORD,
+        },
+      });
+      fireEvent.input(screen.getByPlaceholderText('Confirm Password'), {
+        target: {
+          value: VALID_PASSWORD,
+        },
+      });
+      fireEvent.click(screen.getByText('Sign Up'));
+      await waitFor(() => screen.getByText('Sign Up'));
+      expect(screen.getAllByRole('button')[0]).toHaveTextContent('Sign Up');
+      expect(screen.getByText(ERROR_MESSAGES.INVALID_EMAIL)).toBeInTheDocument();
+    });
+
+    it('catches invalid password', async () => {
+      render(<App />);
+      fireEvent.input(screen.getByPlaceholderText('User'), {
+        target: {
+          value: VALID_EMAIL,
+        },
+      });
+      fireEvent.input(screen.getByPlaceholderText('Password'), {
+        target: {
+          value: `test`,
+        },
+      });
+      fireEvent.input(screen.getByPlaceholderText('Confirm Password'), {
+        target: {
+          value: `test`,
+        },
+      });
+      fireEvent.click(screen.getByText('Sign Up'));
+      await waitFor(() => screen.getByText('Sign Up'));
+      expect(screen.getAllByRole('button')[0]).toHaveTextContent('Sign Up');
+      expect(screen.getByText(ERROR_MESSAGES.PASSWORD_INVALID)).toBeInTheDocument();
+    });
+
+    it('catches passwords not matching', async () => {
+      render(<App />);
+      fireEvent.input(screen.getByPlaceholderText('User'), {
+        target: {
+          value: VALID_EMAIL,
+        },
+      });
+      fireEvent.input(screen.getByPlaceholderText('Password'), {
+        target: {
+          value: `test1234`,
+        },
+      });
+      fireEvent.input(screen.getByPlaceholderText('Confirm Password'), {
+        target: {
+          value: `test4321`,
+        },
+      });
+      fireEvent.click(screen.getByText('Sign Up'));
+      await waitFor(() => screen.getByText('Sign Up'));
+      expect(screen.getAllByRole('button')[0]).toHaveTextContent('Sign Up');
+      expect(screen.getByText(ERROR_MESSAGES.PASSWORD_NOT_MATCHING)).toBeInTheDocument();
+    });
+
     it('moves to login after sign up', async () => {
       render(<App />);
       fireEvent.input(screen.getByPlaceholderText('User'), {
