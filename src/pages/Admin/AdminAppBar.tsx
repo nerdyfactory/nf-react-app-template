ea1a4bdf1992e33405eafd8e2fd1473bf326caac
Nikolas Vasconcelos <nkvdamian@gmail.com>
2021-03-17 17:56:14 -0300

    Admin header navigation style

--- src/pages/Admin/AdminAppBar.tsx
+++ src/pages/Admin/AdminAppBar.tsx
@@ -1,25 +1,39 @@
 import useAuth from 'hooks/useAuth';
-import useBootstrap from 'hooks/useBootstrap';
 import { useHistory } from 'react-router-dom';
 import React, { useCallback } from 'react';
 import { AppBar } from '@material-ui/core';
 import { DefaultButton } from 'components';
-import { MUIColorEnums } from 'constants/enums';
+import { MUIButtonVariantEnums, MUIColorEnums } from 'constants/enums';
 import styled from 'styled-components';
 
 const AdminAppBar: React.ComponentType<{
   title?: string | React.ReactElement<any, string | React.JSXElementConstructor<any>> | undefined;
   open?: boolean | undefined;
   logout?: React.ReactNode;
-}> = (props) => {
+}> = () => {
   const history = useHistory();
   const { logout } = useAuth();
+
+  const handleGoBack = useCallback(() => {
+    history.goBack();
+  }, []);
 
   return (
     <AdminHeader>
       <span>Admin Console</span>
       <div>
-        <DefaultButton muiColor={MUIColorEnums.default} label="Logout" onClick={logout} />
+        <DefaultButton
+          muiColor={MUIColorEnums.inherit}
+          variant={MUIButtonVariantEnums.text}
+          label="Go Back"
+          onClick={handleGoBack}
+        />
+        <DefaultButton
+          muiColor={MUIColorEnums.inherit}
+          variant={MUIButtonVariantEnums.text}
+          label="Logout"
+          onClick={logout}
+        />
       </div>
     </AdminHeader>
   );
